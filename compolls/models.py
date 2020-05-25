b765110cdf7d79a24e7a7c30871cade858a1419b
joffrey <joffrey.armellini@gmail.com>
2020-05-24 23:30:10 -0400

    voters model implemented

--- compolls/models.py
+++ compolls/models.py
@@ -8,12 +8,21 @@ class Question(models.Model):
         return self.question_text
 
 
+class User(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     # choice A, choice B
     choice = models.CharField(max_length=1, default='A')
     choice_text = models.CharField(max_length=300)
-    votes = models.IntegerField(default=0)
+    voters = models.ManyToManyField(User)
 
     def __str__(self):
         return self.choice_text
+
+
--- compolls/views.py
+++ compolls/views.py
@@ -10,10 +10,13 @@ def compoll_question(request):
         pass
     # Else if method GET
     else:
-        user = 'Joffrey'
-        question = Question.objects.get(pk=2)
+        question = Question.objects.all().first()
         choice_a = question.choice_set.filter(choice='A').get()
+        # print(f'Choice A: {choice_a}')
+        # print(f'Choice A voters: {choice_a.voters.all()}')
         choice_b = question.choice_set.filter(choice='B').get()
+        # print(f'Choice B: {choice_b}')
+        # print(f'Choice B voters: {choice_b.voters.all()}')
         context = {'question': question, 'choice_a': choice_a, 'choice_b': choice_b}
     return render(request, 'compolls/question.html', context)
 
